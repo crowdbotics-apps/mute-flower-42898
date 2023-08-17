c8cd24dd26a128135cd9c9c0d933b1a079821e57
Crowdbotics <team@crowdbotics.com>
2023-08-17 07:02:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-flower-42898",
+  url: "https://mute-flower-42898.botics.co",
+  api: "https://mute-flower-42898.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
